57cb28fe4c00b2bd031d23128fcb53c2deefd06a
Lyes <lyes.dabla@gmail.com>
2024-06-01 11:11:39 +0200

    Decoding of the damn torrent file

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 import fs from "fs";
 import bencode from "bencode";
-const torrent = bencode.decode(fs.readFileSync("puppy.torrent"));
-console.log(torrent.announce.toString("utf8"));
+
+const torrent = bencode.decode(fs.readFileSync("puppy.torrent"), "utf8");
+
+console.log(torrent.announce);
