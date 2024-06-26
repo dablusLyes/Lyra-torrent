338b6c4c5a81378b383ac21a5399716a5aae7b1d
Lyes Ramdane <lyes.dabla@gmail.com>
2024-06-26 12:55:53 +0200

    ll

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 import fs from "fs";
 import bencode from "bencode";
+import { Server } from "socketio";
+import { parse } from "url";
 
 const torrent = bencode.decode(fs.readFileSync("puppy.torrent"), "utf8");
+
+url = parse(torrent.announce);
+
+const socket = dgram.createSocket("udp4");
 
 console.log(torrent.announce);
